e16117748bfe6ea740e9f02b8d0246b98502a7f1
Eyevou <40645381+Eyevou@users.noreply.github.com>
2026-02-22 01:49:22 -0500

    Delete ActionTargetAutoSwap directory

--- ActionTargetAutoSwap/Locale/deDE.lua
+++
@@ -1,7 +0,0 @@
--- Deutsch(deDE) Localization by BrilliantYami
-
-if(GetLocale() == "deDE")then
-	--print("|cffFFC125Sprache eingestellt auf:|r Deutsch")
-	ATAS_softTargetEnemy_Enabled     = "|cffFFC125Zielerfassung für Aktionen ist nun:|r |cff00ff00AKTIVIERT!!|r"
-	ATAS_softTargetEnemy_Disabled    = "|cffFFC125Zielerfassung für Aktionen ist nun:|r |cffFF0000DEAKTIVIERT!|r"
-end
--- ActionTargetAutoSwap/Locale/esMX.lua
+++
@@ -1,7 +0,0 @@
--- English(GB) Localization by Eyevou (Patreon: https://www.patreon.com/Eyevou)
-
-if(GetLocale() == "esMX")then
-	--print("|cffFFC125Localización establecida en:|r Español(esMX)")
-	ATAS_softTargetEnemy_Enabled     = "|cffFFC125Action Targeting is now:|r |cff00ff00ENABLED!|r"
-	ATAS_softTargetEnemy_Disabled    = "|cffFFC125Action Targeting is now:|r |cffFF0000DISABLED!|r"
-end
